1e1b6ad35d38a071c188dfec20217f49db0502c0
Marcos Sombra <markos.vs@live.com>
2018-12-27 13:45:30 -0300

    Migração completa para Python + add icone de logo temporario

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# paginas user
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -16,6 +19,24 @@ def sobre():
 @app.route('/contato/')
 def contato():
     return render_template('contato.html')
+
+
+# paginas adm
+
+
+@app.route('/admin/')
+def adm_index():
+    return render_template('admin/index.html')
+
+
+@app.route('/admin/novo-post/')
+def adm_novo_post():
+    return render_template('admin/novo-post.html')
+
+
+@app.route('/admin/login/')
+def adm_login():
+    return render_template('admin/login.html')
 
 
 if __name__ == '__main__':
